2d7374f6b64b8d91856da6ac120d13701dbb1038
Adrian Gomez <agomezrodr@miners.utep.edu>
2019-09-01 22:32:03 -0600

    Cleaning code and add links

--- wordCount.py
+++ wordCount.py
@@ -1,15 +1,19 @@
 # Python intro
 # Adrian Gomez Rodriguez 80625896
-#8/31/2019
+# 8/31/2019
+# Dr. Daniel Cervantes
+# Theory of Operating Systems.
 
-import sys        # command line arguments
-import re         # regular expression tools
-import os         # checking if file exists
+import sys  # command line arguments
+import re   # regular expression tools
+import os   # checking if file exists
 
+#if to check that there are 3 arguments when running programm
 if len(sys.argv) < 3:
     print("To run program, type wordCount.py <inputFileName> <outputFileName>")
     exit()
 
+#Declaration
 inFile = sys.argv[1]  #declaring input variable
 outFile = sys.argv[2]  #declaring output variable
 container = {}   # Here is where Words and Counts will be stored.
@@ -19,12 +23,13 @@ if not os.path.isfile(inFile):
     print ("The file %s doesn't exist in the source path" % inFile)
     exit()
 
-
+#open and read file  (3)
 with open(inFile, 'rt') as text:
     for line in text:                       
-        line = line.strip() 
+        line = line.strip()  # .strip() remove all the leading and trailing spaces from a string. (1)
         # print("HERE......") 
-        for words in re.split("['\s\t.,-;:=]", line):
+        for words in re.split("['\s\t.,-;:=]", line):  #
+            # lower() returns a copy of the string in which all case-based characters have been lowercased. (2)
             if words.lower() == "":  
                 continue           
                      #    print("CONTINUE......") 
@@ -36,7 +41,7 @@ with open(inFile, 'rt') as text:
                     #     print("CLOSE") 
 
                 
-#sorting and writting in the output file.
+#sorting and writting in the output file. (4)(5)
 output = open(outFile,"w+")
 #   print("Openning.....") 
 for words, num in sorted(container.items()):
@@ -45,23 +50,8 @@ for words, num in sorted(container.items()):
 print 'Output File Ready: %s' % outFile
 output.close()
 
-
-# try:
-#     print('Openning file: %s - counting words...' % inputFile)
-#     f = open(inputFile, 'r')
-#     x = f.read()
-#     list1 = re.findall(r'\w+', x, flags=re.I)
-#     for words in list1:
-#         words = words.lower()
-#          if word in list2.key():
-#             list2[words] +=1
-#             else:
-#                 list2[words] = 1
-#     f.close()
-# output = open(outputFile, 'w+')
-# for words in sorted(list2):
-#     output.write('%s %s \n' % (words, list2[words]))
-#     print 'Output file: %s' %outputFile
-# except FileNotFoundError:
-#     print'Input File does not exist: %s' % inputFile
-#     exit() 
+# (1) Refrenced: https://www.geeksforgeeks.org/python-string-strip-2/
+# (2) Refrenced: https://www.tutorialspoint.com/python/string_lower.htm
+# (3) Refrenced: https://stackoverflow.com/questions/16922214/reading-a-text-file-and-splitting-it-into-single-words-in-python
+# (4) Refrenced: https://www.guru99.com/reading-and-writing-files-in-python.html
+# (5) Refrenced: https://docs.python.org/2/howto/sorting.html
